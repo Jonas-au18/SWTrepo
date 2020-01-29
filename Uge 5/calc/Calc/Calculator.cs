f652b9590f555e5cecacbb87d5a2b7b4eca0b061
au293673 <au293673@uni.au.dk>
2020-01-29 13:13:41 +0100

    First commit

---
+++ Uge 5/calc/Calc/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Calculator
+    {
+        public Calculator()
+        { }
+        
+        public double Add(double a, double b)
+        {
+            double result = a + b;
+            return result;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            double result = a - b;
+            return result;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            double result = a * b;
+            return result;
+        }
+
+        public double Power(double a, double b)
+        {
+            double result = Math.Pow(a, b);
+            return result;
+        }
+    }
+}
---
+++ Uge 5/calc/Calc/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Calculator calc = new Calculator();
+                double left;
+                double right;
+                string option = "";
+                double.TryParse(Console.ReadLine(), out left);
+                option = Console.ReadLine();
+                double result;
+
+                switch (option)
+                {
+                    case "+":
+                    {
+                        double.TryParse(Console.ReadLine(), out right);
+                        result = calc.Add(left, right);
+                        Console.WriteLine(result);
+                        break;
+                    }
+                    case "-":
+                    {
+                        double.TryParse(Console.ReadLine(), out right);
+                        result = calc.Subtract(left, right);
+                        Console.WriteLine(result);
+                        break;
+                    }
+                    case "p":
+                    {
+                        double.TryParse(Console.ReadLine(), out right); 
+                        result = calc.Power(left, right);
+                        Console.WriteLine(result);
+                            break;
+                    }
+                    case "*":
+                    {
+                        double.TryParse(Console.ReadLine(), out right);
+                        result = calc.Multiply(left, right);
+                        Console.WriteLine(result);
+                            break;
+                    }
+                    default:
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
